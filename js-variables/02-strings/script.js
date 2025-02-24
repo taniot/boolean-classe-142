077f4daeb6121195842c88e43884078709f6ea23
taniot <gaetano.frascolla@gmail.com>
2025-02-24 10:27:34 +0100

    feat: solved second exercise

---
+++ js-variables/02-strings/script.js
@@ -0,0 +1,38 @@
+// 1. Dichiarazione di variabili
+// Crea una variabile chiamata name e assegnale il tuo nome.
+// Stampa il valore di nome nella console.
+
+const myName = 'Gaetano'; //stringa => Gaetano
+console.log(myName);
+
+// 2. Tipi primitivi
+// Crea tre variabili chiamate number, text e bool e assegna rispettivamente
+// un numero, una stringa e un valore booleano.
+// Stampa tutti i valori nella console.
+
+const number = 42; //Number
+const text = 'Il mio testo'; //String
+const bool = true; //Boolean
+
+// console.log('Numero: ', number, 'Stringa: ',text, 'Bool: ',bool);
+
+console.log(number);
+console.log(text);
+console.log(bool);
+
+// 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
+// e le variabili sopra in modo da stampare correttamente 
+// in console la stringa con il nome completo
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
+const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
+const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
+
+const fullName = firstName + ' '+ lastName;
+console.log(fullName); // Output: "Jane Smith"
+
+
+
+// 4. Imposta questa variabile utilizzando una proprietà
+// sulla variabile alphabet per ottenere la sua lunghezza
+const numberOfLettersInAlphabet = alphabet.length;
+console.log(numberOfLettersInAlphabet); // Output: 26
