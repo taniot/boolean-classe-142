4d577cf75c362f65b2ad74e61bef67baad3cbca3
taniot <gaetano.frascolla@gmail.com>
2025-02-25 15:28:49 +0100

    add: for

---
+++ js-for/js/app.js
@@ -0,0 +1,124 @@
+
+// console.log('Gaetano');
+
+// if () {
+
+// }
+
+
+//primo ciclo for
+
+// console.log('prima del ciclo');
+
+// for (let contatore = 0; contatore <= 10; contatore++) {
+//   console.log(contatore);
+//   console.log('Gaetano');
+
+//   //incremento la variabile contatore => 10
+// }
+
+
+// console.log('dopo il ciclo');
+
+
+
+
+//raffaele
+// console.log('soluzione raffaele');
+// //fare un contatore che conta da 10 a 1
+// for (let i = 1; i >= 10; i--) {
+//   console.log(i);
+//   // console.log('Gaetano');
+//   // console.log('Giovanni');
+
+//   //incremento la variabile contatore => 10
+// }
+// console.log('---');
+// //enrico
+// console.log('soluzione enrico');
+
+
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+//   // console.log('Gaetano');
+//   // console.log('Giovanni');
+
+//   //incremento la variabile contatore => 10
+// }
+
+// //marco
+
+// console.log('---');
+// //enrico
+// console.log('soluzione marco');
+
+
+// for (let i = 10; i > 0; i--) {
+//   console.log(i);
+//   // console.log('Gaetano');
+//   // console.log('Giovanni');
+
+//   //incremento la variabile contatore => 10
+// }
+
+// //andrea
+
+// console.log('---');
+// //enrico
+// console.log('soluzione andrea');
+
+// let x = 10;
+
+// for (let i = 10; i < 1; i--) {
+
+
+//   x = i;
+//   console.log(i);
+//   // console.log('Gaetano');
+//   // console.log('Giovanni');
+
+//   //incremento la variabile contatore => 10
+// }
+
+// //esteban
+
+// console.log('---');
+// //enrico
+// console.log('soluzione esteban');
+
+
+// for (let i = 10; i > -10; i--) {
+//   console.log(i);
+//   // console.log('Gaetano');
+//   // console.log('Giovanni');
+
+//   //incremento la variabile contatore => 10
+// }
+
+//stampare i numeri pari da 0 a 10
+for (let i = 0; i <= 10; i = i + 2) {
+  console.log(i)
+}
+
+console.log('---');
+
+//alternativa
+for (let i = 0; i <= 10; i++) {
+
+  // console.log(i);
+  // console.log(i % 2);
+
+  if (i % 2 === 0) {
+    console.log(i);
+  } else {
+    console.log('valore dispari', i)
+  }
+
+
+
+
+  // if(i % 2){
+  //   console.log(i);
+  // }
+
+}
