98cf2b4bffd65cb8c56400224a087f4cb08883e2
taniot <gaetano.frascolla@gmail.com>
2025-02-26 09:58:19 +0100

    feat: create iteration from 1 to 100

--- js-fizzbuzz/js/app.js
+++ js-fizzbuzz/js/app.js
@@ -1,1 +1,23 @@
 console.log('Ciao Gaetano');
+
+/*
+## Pseudocodice
+
+- scrivo un ciclo for che parte da 1 e arriva a 100 (index === indice)
+- per ogni valore dell'indice eseguo condizioni specifiche
+- SE indice è divisibile per 3
+  - stampo fizz
+- ALTRIMENTI SE indice è divisibile per 5
+  - stampo buzz
+- ALTRIMENTI SE indice è divisibile per 3 e per 5
+  - stampo fizzbuzz
+- ALTRIMENTI 
+  - stampo indice
+
+*/
+
+for (let index = 1; index <= 100; index++) {
+  //eseguo il codice tra parentesi
+  //incremento index => index++
+}
+
