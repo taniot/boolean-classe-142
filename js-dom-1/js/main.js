9de07a708ad1c1b20579bfc7686693d74663daf4
taniot <gaetano.frascolla@gmail.com>
2025-03-05 10:05:43 +0100

    feat: enlight bulb

--- js-dom-1/js/main.js
+++ js-dom-1/js/main.js
@@ -5,3 +5,24 @@ Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato 
 Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
 
 */
+
+//immagine
+const bulbElement = document.getElementById('bulb');
+console.log(bulbElement);
+
+//bottone
+const buttonElement = document.getElementById('switch');
+console.log(buttonElement);
+
+buttonElement.addEventListener('click', function () {
+  // console.log('ho cliccato sul bottone');
+
+  // console.log(bulbElement.src);
+  // console.log(bulbElement.id);
+  // console.log(bulbElement.alt);
+
+  bulbElement.src = 'img/yellow_lamp.png';
+  bulbElement.alt = 'Lampadina accesa';
+
+
+});
