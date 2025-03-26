14901cf52d2620aa2605f133e7e58b8112b7f114
taniot <gaetano.frascolla@gmail.com>
2025-03-26 10:21:08 +0100

    add: solution with operator

--- node-hello-world/index.js
+++ node-hello-world/index.js
@@ -1,1 +1,46 @@
-console.log('Hello World');
+
+
+// console.log(process.argv[2]);
+
+
+
+//operatore ternario
+//const message = process.argv[2] ? process.argv[2] : "Hello World";
+
+//null coalescing operator NULL or UNDEFINED
+//const message = process.argv[2] ?? "Hello World";
+//or operator
+// const message = process.argv[2] || "Hello World";
+
+// if (process.argv[2]) {
+//   console.log(...process.argv.slice(2));
+// } else {
+//   console.log("Hello World");
+// }
+
+
+
+// let messagee;
+// if (process.argv[2] != null) {
+//   message = process.argv[2];
+// } else {
+//   message = "Hello World"
+// }
+
+
+/*
+&& => operatore AND
+|| => operatore OR
+! => operatore NOT
+*/
+
+
+
+// console.log(message);
+
+
+
+console.log("" || "arrivederci");
+
+console.log("" ?? "arrivederci");
+
